b7057b20f13c96cb7bec1b2679b40c3b42a39d96
Manzurur Khan <iamsidky@gmail.com>
2019-03-09 13:19:29 -0800

    endpoint

--- main.go
+++ main.go
@@ -1,10 +1,17 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"log"
+)
 
 func main() {
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H { "message": "pong" })
+	})
+	r.POST("/feed/update", func(c *gin.Context) {
+		log.Print(c.Request.Body)
 		c.JSON(200, gin.H { "message": "pong" })
 	})
 	r.Run()
